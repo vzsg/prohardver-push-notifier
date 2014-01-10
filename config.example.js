b4ba4a7c0a2ff80103ea55362f6bfb9bafb527b9
Zsolt Váradi <zsolt.varadi@outlook.com>
2014-01-10 05:45:35 -0500

    Cleaned up the example configuration file.

--- config.example.js
+++ config.example.js
@@ -1,15 +1,16 @@
 /**
  * Available Sites:
  *   prohardver:
- *     username, password
- *     subsite
- *     mobile
- *     favoriteThreadsOnly
+ *     username, password - credentials for the PH! sites
+ *     subsite - PH! flavor in the links: prohardver, mobilarena, itcafe, logout or gamepod
+ *     mobile - set to true if you want mobile-friendly links
+ *     includeRecent - set to true if you want notifications for your recent threads too, not just favorites
  *
- * Available formats:
- *   list
- *   count
- *   urls
+ * Common settings:
+ *   checkInterval - number of minutes to wait between polling the forum
+ *   notifyTo - a single or a list of device identifiers to send pushes to; 'all' and true (boolean) are jokers
+ * 
+ * Be sure to copy your PushBullet API key from https://pushbullet.com/account!
  */
 
 var Configuration = {
@@ -19,7 +20,6 @@ var Configuration = {
             'username': 'Your Prohardver username',
             'password': 'Your Prohardver password',
             'checkInterval': 10, // In minutes
-            'format': 'urls',
             'notifyTo': 'all',
             'subsite': 'prohardver',
             'mobile': false,
